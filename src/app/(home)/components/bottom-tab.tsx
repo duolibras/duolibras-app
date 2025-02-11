cef2853d1d1be1842497d18baf304888f1cfb2b0
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2025-02-11 10:25:53 -0300

    add

--- src/app/(home)/components/bottom-tab.tsx
+++ src/app/(home)/components/bottom-tab.tsx
@@ -1,18 +1,44 @@
-import { Button } from "@/components/ui/button";
+"use client"
+
 import Image from "next/image"
+import { useState } from "react";
+
 import { Assets } from "@/shared/images"
+import { Button } from "@/components/ui/button";
 
 export function BottomTab() {
+  const [selected, setSelected] = useState({
+    trilha: true,
+    content: false
+  });
+
+  const [effect, setEffect] = useState({
+    trilha: true,
+    content: false
+  });
+
+  const toogleTab = (tab: string) => {
+    setEffect({
+      trilha: tab === "trilha" && !effect.trilha,
+      content: tab === "content" && !effect.content
+    })
+
+    setSelected({
+      trilha: tab === "trilha" && !selected.trilha,
+      content: tab === "content" && !selected.content
+    })
+  }
+
   return (
     <footer className="w-full fixed bottom-0 flex justify-around items-center bg-gray-50 h-20">
-        <Button className="w-full flex flex-col bg-transparent hover:bg-transparent">
-          <Image src={Assets.SVG.TrilhaIcon} width={40} height={40} alt="Trilha" />
-          <span className="text-gray-700">TRILHA DE ESTUDO</span>
-        </Button>
-        <Button className="w-full flex flex-col bg-transparent hover:bg-transparent">
-          <Image src={Assets.SVG.ContentIcon} width={40} height={40} alt="Content"/>
-          <span className="text-gray-500 font-normal">CONTEÚDO EXCLUSIVO</span>
-        </Button>
-      </footer>
+      <Button className={`w-full flex flex-col bg-transparent hover:bg-transparent ${effect.trilha && "animate-wiggle"}`} onClick={() => toogleTab("trilha")}>
+        <Image src={Assets.SVG.TrilhaIcon} width={40} height={40} alt="Trilha" />
+        <span className={`${selected.trilha ? 'text-gray-700 ' : 'text-gray-500 font-normal'}`}>TRILHA DE ESTUDO</span>
+      </Button>
+      <Button className={`w-full flex flex-col bg-transparent hover:bg-transparent ${effect.content && "animate-wiggle"}`} onClick={() => toogleTab("content")}>
+        <Image src={Assets.SVG.ContentIcon} width={40} height={40} alt="Content" />
+        <span className={`${selected.content ? 'text-gray-700 ' : 'text-gray-500 font-normal'}`}>CONTEÚDO EXCLUSIVO</span>
+      </Button>
+    </footer>
   )
 }
--- src/app/(home)/content-selected/[contentId]/page.tsx
+++ src/app/(home)/content-selected/[contentId]/page.tsx
@@ -1,46 +1,32 @@
 import { Header } from "../../components/header";
 import { ContentCard } from "../../exclusive-content/components/content-card";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
-import Image from "next/image";
-import Curso from "@/assets/images/course.avif";
 import { CircleX } from "lucide-react";
+import { MiniCardContent } from "./components/mini-card-content";
 
 export default function ContentSelected() {
   return (
-    <div className="w-full flex flex-col items-center h-screen max-w-lg mx-auto bg-white py-6 px-6">
+    <div className="w-full flex flex-col items-center h-fit max-w-lg mx-auto bg-white py-6 px-6">
       <Header />
       <br />
       <main className="w-full">
         <ContentCard />
         <br />
-        <div className="w-full flex flex-col gap-4 border p-4 rounded-lg">
-          <div>
-            <div className="flex space-between">
+        <div className="w-full flex flex-col gap-4 border py-3 rounded-lg">
+            <div className="flex space-between px-4">
               <div className="w-full">
-                <h4 className="text-xl font-medium">Mais conteúdo</h4>
-                <p>jfrjfirngtngvirtugjigjrgroi</p>
+                <h4 className="text-xl font-medium">Mais conteúdos</h4>
+                <p className="text-gray-400">jfrjfirngtngvirtugjigjrgroi</p>
               </div>
-              <CircleX className="hover:text-red-500 cursor-pointer transition-all duration-500" />
+              <CircleX className="text-gray-300 hover:text-red-500 cursor-pointer transition-all duration-500 " />
             </div>
-            <br />
-            <div className="w-full flex">
-              <AspectRatio ratio={16 / 9} className="w-48 h-24 bg-muted">
-                <Image
-                  src={Curso}
-                  alt="Photo by Drew Beamer"
-                  fill
-                  className="w-full object-cover"
-                />
-              </AspectRatio>
-              <div className="w-full flex flex-col gap-2">
-                <h5 className="text-gray-800 ">Titulo</h5>
-                <p>Descrição</p>
-              </div>
-            </div>
-          </div>
+            <div>
+              <MiniCardContent selected />
+              <MiniCardContent />
+              <MiniCardContent />
+              <MiniCardContent />
+           </div>
         </div>
       </main>
-
     </div>
   )
 }
