7b88f754a0094377e890ef6d5a8581f517eb5a2c
Huandrey Pontes <huandrey.pontes@ccc.ufcg.edu.br>
2025-02-12 22:10:16 -0300

    Feat/conteudos (#9)
    * .
    
    * pwa
    
    * .

--- src/shared/store/mocks/chapters.ts
+++ src/shared/store/mocks/chapters.ts
@@ -4,8 +4,8 @@ import { lessonsUnit1 } from "./lessons";
 export const chaptersUnit1 = [
   {
     id: '1',
-    name: 'Vamos aprender?',
-    description: 'Descrição do vamos aprender',
+    name: 'Fundamentos essenciais',
+    description: 'Saudações e Cumprimentos',
     lessons: lessonsUnit1,
     lessonsAccomplished: 2,
     lessonsCount: 5,
@@ -13,8 +13,35 @@ export const chaptersUnit1 = [
   },
   {
     id: '2',
-    name: 'Como estamos seguindo?',
-    description: 'Descrição do vamos aprender',
+    name: 'Alfabeto',
+    description: 'Pratique a datilologia para soletrar palavras.',
+    lessons: [],
+    lessonsAccomplished: 0,
+    lessonsCount: 5,
+    status: Status.UNAVAILABLE
+  },
+  {
+    id: '2',
+    name: 'Pronomes e Pessoas',
+    description: 'Estude os pronomes pessoais e sua aplicação.',
+    lessons: [],
+    lessonsAccomplished: 0,
+    lessonsCount: 5,
+    status: Status.UNAVAILABLE
+  },
+  {
+    id: '3',
+    name: 'Cores',
+    description: 'Explore os sinais das cores principais e suas variações',
+    lessons: [],
+    lessonsAccomplished: 0,
+    lessonsCount: 5,
+    status: Status.UNAVAILABLE
+  },
+  {
+    id: '4',
+    name: 'Verbos',
+    description: 'Domine os verbos mais utilizados no dia a dia.',
     lessons: [],
     lessonsAccomplished: 0,
     lessonsCount: 5,
